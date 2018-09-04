3bb3c3e8c2d23a3d5a6c7c5198777499d4bee21f
sambapython <sambapython@gmail.com>
2018-09-04 09:24:10 +0530

    2018-09-04 modules

---
+++ modules/app.py
@@ -0,0 +1,21 @@
+'''
+#create_customer()
+b=20
+print a
+'''
+'''
+print "program started"
+b=20
+import sales
+print b
+print "program ended"
+'''
+'''
+import sales
+print a
+'''
+b=20
+import sales
+print b
+print sales.a
+sales.fun()
---
+++ modules/sales.py
@@ -0,0 +1,5 @@
+a=10
+def fun():
+	print "thi is fun"
+fun()
+print a
